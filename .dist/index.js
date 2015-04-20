d26fe89c6215a0312999316691de69e1bce9885c
Blake La Pierre <blakelapierre@gmail.com>
2015-04-19 23:13:16 -0700

    Update index to expose each function

--- .dist/index.js
+++ .dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
-module.exports = require('rtc.js');
+var $__rtc__,
+    $__signaler__,
+    $__peer__,
+    $__channel__,
+    $__stream__;
+var rtc = ($__rtc__ = require("./rtc"), $__rtc__ && $__rtc__.__esModule && $__rtc__ || {default: $__rtc__}).default;
+var signaler = ($__signaler__ = require("./signaler"), $__signaler__ && $__signaler__.__esModule && $__signaler__ || {default: $__signaler__}).default;
+var peer = ($__peer__ = require("./peer"), $__peer__ && $__peer__.__esModule && $__peer__ || {default: $__peer__}).default;
+var channel = ($__channel__ = require("./channel"), $__channel__ && $__channel__.__esModule && $__channel__ || {default: $__channel__}).default;
+var stream = ($__stream__ = require("./stream"), $__stream__ && $__stream__.__esModule && $__stream__ || {default: $__stream__}).default;
+module.exports = {
+  rtc: rtc,
+  signaler: signaler,
+  peer: peer,
+  channel: channel,
+  stream: stream
+};
 
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-module.exports = require('rtc.js');
+import rtc from './rtc';
+import signaler from './signaler';
+import peer from './peer';
+import channel from './channel';
+import stream from './stream';
+
+module.exports = {
+  rtc,
+  signaler,
+  peer,
+  channel,
+  stream
+};
